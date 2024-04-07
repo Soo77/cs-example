c319883bd2020a1868b221a0d5e54b6b5a1bc09a
Soo77 <shimsh3@naver.com>
2024-04-07 21:46:16 +0900

    Initial commit

---
+++ src/main/java/hello/csexample/CsExampleApplication.java
@@ -0,0 +1,13 @@
+package hello.csexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/csexample/CsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.csexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
